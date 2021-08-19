0b0d4427ab4d86bbc04ea8f79145ae8bf7945369
NatiaSva <55664887+NatiaSva@users.noreply.github.com>
2021-08-19 14:47:13 +0300

    1

---
+++ Microservices.WebApi/Customer.Microservices/Startup.cs
@@ -0,0 +1,90 @@
+using Customer.Microservices.Consumers;
+using GreenPipes;
+using MassTransit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customer.Microservices
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<ProductConsumer>();
+                x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cur =>
+                {
+                    cur.UseHealthCheck(provider);
+                    cur.Host(new Uri("rabbitmq://localhost"), h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                    cur.ReceiveEndpoint("productQueue", oq =>
+                    {
+                        oq.PrefetchCount = 20;
+                        oq.UseMessageRetry(r => r.Interval(2, 100));
+                        oq.ConfigureConsumer<ProductConsumer>(provider);
+                    });
+                }));
+            });
+            services.AddMassTransitHostedService();
+
+
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Customer.Microservice", Version = "v1" });
+            });
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer.Microservice v1");
+                    c.RoutePrefix = string.Empty;
+                });
+               
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ Microservices.WebApi/Product.Microservices/Controllers/CustomerProductController.cs
@@ -0,0 +1,38 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product.Microservices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerProductController : ControllerBase
+    {
+        private readonly IBus _busService;
+        public CustomerProductController(IBus busService)
+        {
+            _busService = busService;
+        }
+
+        [HttpPost]
+        public async Task<string> CreateProduct(Shared.Models.Models.CustomerProduct user)
+        {
+            if (user != null)
+            {
+                user.Date = DateTime.Now;
+                user.Age = 0;
+                Uri uri = new Uri("rabbitmq://localhost/productQueue");
+                var endPoint = await _busService.GetSendEndpoint(uri);
+                await endPoint.Send(user);
+                return "true";
+            }
+            return "false";
+        }
+
+
+    }
+}
